fdffc64bf600d08a6427ce185150a4c8b4630097
MChristian00 <manzichristian07@gmail.com>
2020-10-30 10:43:54 +0200

    Added build script in package.json

--- build/Routes/UserRoutes.js
+++ build/Routes/UserRoutes.js
@@ -1,32 +1,40 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+  value: true,
 });
 exports["default"] = void 0;
 
 var _express = require("express");
 
-var _UserControllers = _interopRequireDefault(require("../Controllers/UserControllers"));
+var _UserControllers = _interopRequireDefault(
+  require("../Controllers/UserControllers")
+);
 
 var _User = _interopRequireDefault(require("../Middlewares/Validation/User"));
 
 var _CheckAuthToken = require("../Middlewares/CheckAuthToken");
 
-var _checkAdmin = require("../Middlewares/checkAdmin");
+var _checkAdmin = require("../Middlewares/CheckAdmin");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
 
 var router = (0, _express.Router)();
 var registerFormValidation = _User["default"].registerFormValidation,
-    loginFormValidation = _User["default"].loginFormValidation;
+  loginFormValidation = _User["default"].loginFormValidation;
 var register = _UserControllers["default"].register,
-    login = _UserControllers["default"].login,
-    updateProfile = _UserControllers["default"].updateProfile,
-    logout = _UserControllers["default"].logout;
+  login = _UserControllers["default"].login,
+  updateProfile = _UserControllers["default"].updateProfile,
+  logout = _UserControllers["default"].logout;
 router.post("/register", registerFormValidation, register);
 router.post("/signin", loginFormValidation, login);
-router.put("/edit/:id", [_CheckAuthToken.checkAuthToken, _checkAdmin.checkAdmin], updateProfile);
+router.put(
+  "/edit/:id",
+  [_CheckAuthToken.checkAuthToken, _checkAdmin.checkAdmin],
+  updateProfile
+);
 router.get("/logout", _CheckAuthToken.checkAuthToken, logout);
 var _default = router;
 exports["default"] = _default;
